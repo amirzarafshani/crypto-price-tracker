6ec861ae8db5830cdccf696bb547a03f6beb8f6f
amirzarafshani <amirzarafshani@gmail.com>
2023-05-10 19:16:09 +0300

    Install and config tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-  return <div className="App">Crypto Price Tracker</div>;
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Crypto Price Tracker</h1>
+    </div>
+  );
 }
 
 export default App;
